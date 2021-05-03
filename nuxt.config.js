4bbbcfa811ef610bbe782795bb91329f8fb4c94a
Shinya Fujino <shf0811@gmail.com>
2021-05-03 17:54:11 +0900

    Finish "Getting Started"

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,9 +23,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/composition-api/module',
-  ],
+  buildModules: ['@nuxtjs/composition-api/module'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
